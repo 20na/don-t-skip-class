1a7788a2f3baa9a5c4520daf853515dc5c462d6d
dannyvillamil <vilde-26@rhodes.edu>
2024-11-12 11:54:06 -0600

    playermovement (only UI management, not actualy movement)

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    public GameObject[] notebooks;
+    private int currentBookIndex = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Activate the first notebook UI
+        notebooks[currentBookIndex].SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Check if if theres a next notebook
+        if (currentBookIndex < notebooks.Length - 1)
+        {
+            //Deactivate current notebook
+            notebooks[currentBookIndex].SetActive(false);
+
+            //Move to the next notebook
+            currentBookIndex++;
+            notebooks[currentBookIndex].SetActive(true);
+
+        }
     }
 }
