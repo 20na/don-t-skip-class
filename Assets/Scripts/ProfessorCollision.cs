dd390d3f0e61d2f56a03243348d0776dba233102
Ouyel-26 <ouyellen166@gmail.com>
2024-12-13 22:14:13 -0600

    Edited Professor Collision
    Kinda struggling to work but want to see if it works upon collision in vr

--- Assets/Scripts/ProfessorCollision.cs
+++ Assets/Scripts/ProfessorCollision.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class ProfessorCollision : MonoBehaviour
 {
+
+    public GameObject gameOverScreen;
+   // public GameObject ProfessorBots;
+    private GameObject scheduleController;
+    private Schedule scheduleScript;
+    //public GameObject[] Prof;
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+      public void OnCollisionEnter(Collision collision)
     {
+        if(collision.gameObject.CompareTag("Professor")){
+            Debug.Log("Contact Professor");
+            gameOverScreen.SetActive(true);
+            StartCoroutine(Coroutine1());
+        //Ending game (deactivating schedule and professors)
+            scheduleScript.endGame();
+        }
+        
         
     }
+
+    IEnumerator Coroutine1()
+    {
+        yield return new WaitForSeconds(2);
+        gameOverScreen.SetActive(false);
+    }
+
 }
